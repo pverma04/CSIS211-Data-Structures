044c649db43cd3a09d358f9d2315a3a5a87772bf
pverma04 <parthverma1104@gmail.com>
2022-07-14 19:36:46 -0700

    Updated with comments

--- Unit7_1/src/unit7/HashTable.java
+++ Unit7_1/src/unit7/HashTable.java
@@ -1,3 +1,12 @@
+/**
+ * ***********************************************
+ * File: Main.java
+ * Author: Parth Verma
+ * Description: This file contains code for the HashTable class
+ * Date: July 14, 2022
+ * ***********************************************
+ */
+
 package unit7;
 import java.util.ArrayList;
 
@@ -12,61 +21,86 @@ public class HashTable<T> {
             mData.add(null);
         }
     }
+    
+    /**
+     * ***********************************************
+     * @par Name hashFunc
+     * @purpose Determines the input position of a given string by finding the
+     * hashcode of the string MOD by size of the table
+     * @param [in] : String str
+     * @return Int***********************************************
+     */
     public int hashFunc (String str) {
         return Math.abs(str.hashCode() % mTABLE_SIZE);
     }
     
+    /**
+     * ***********************************************
+     * @par Name add
+     * @purpose Creates and adds a Pair to the HashTable, given the data first and second of the pair
+     * @param [in] : String name, String number
+     * @return Void***********************************************
+     */
     public void add(String name, String number) {
         Pair addOn = new Pair(name, number);
         int pos = hashFunc(name);
-        //System.out.println(name + " hash: " + pos);
         PairList<Pair> listAtPos = null;
-        if (mData.get(pos) == null) {
+        if (mData.get(pos) == null) { //if pos holds an empty PairList, create a new one with the new data
             listAtPos = new PairList<Pair>();
             listAtPos.addPair(addOn);
             mData.set(pos, listAtPos);
-        } else {
-            listAtPos = mData.get(pos);
+        } else { //hold the PL at pos and adds to it, and resetts the PL at pos to the updated one
+            listAtPos = mData.get(pos); 
             listAtPos.addToEnd(addOn);
             mData.set(pos, listAtPos);
         }
     }
+    
+    /**
+     * ***********************************************
+     * @par Name lookup
+     * @purpose Searches for the given name in the HashTable and returns all
+     * data associated with the name
+     * @param [in] : String name
+     * @return String***********************************************
+     */
     public String lookup(String name) {
         int pos = hashFunc(name);
-        PairList<Pair> list = mData.get(pos);
-        if(list == null){
+        PairList<Pair> list = mData.get(pos); //PairList at the needed index
+        if(list == null){ //if null, name does not exist in the HT
             return null;
         } else {
-            //return "";
             Node p = list.getNode(0); //head node
-            while (p.mNext != null) {
-               //System.out.println(((Pair) p.mData).getFirst());
-               if (((Pair)p.mData).getFirst().equals(name)) {
-                   //return "" + ((Pair) p.mData).getSecond();
+            while (p.mNext != null) { //traverse
+               if (((Pair)p.mData).getFirst().equals(name)) { //if name has been found
                    return ((Pair) p.mData).getFirst() + "(" + hashFunc("" + ((Pair) p.mData).getFirst()) + "-" + p.mOffset + ")" + " " + ((Pair) p.mData).getSecond();
                }
                p = p.mNext;
             }
-            if (((Pair) p.mData).getFirst().equals(name)) {
-                //return "" + ((Pair) p.mData).getSecond();
+            if (((Pair) p.mData).getFirst().equals(name)) { //for the last Pair in the PL
                 return ((Pair) p.mData).getFirst() + "(" + hashFunc("" + ((Pair) p.mData).getFirst()) + "-" + p.mOffset + ")" + " " + ((Pair) p.mData).getSecond();
             }
             return "Name not found";
         }
-    }    
+    }  
+    
+    /**
+     * ***********************************************
+     * @par Name printTable
+     * @purpose Traverses through and prints data in the HT
+     * @param [in] : None
+     * @return Void***********************************************
+     */
     public void printTable() {
-        //int offset = 0;
         for (PairList pL : mData) {
             if (pL != null) {
                 Node p = pL.getNode(0);
                 while (p.mNext != null) {
                     System.out.println(((Pair) p.mData).getFirst() + "(" + hashFunc("" + ((Pair) p.mData).getFirst()) + "-" + p.mOffset + ")" + "; " + ((Pair) p.mData).getSecond() + ";");
-                    //offset++;
                     p = p.mNext;
                 }
                 System.out.println(((Pair) p.mData).getFirst() + "(" + hashFunc("" + ((Pair) p.mData).getFirst()) + "-" + p.mOffset + ")" + "; " + ((Pair) p.mData).getSecond() + ";");
             }
-            //offset = 0;
         }
     }
 }
--- Unit7_1/src/unit7/Main.java
+++ Unit7_1/src/unit7/Main.java
@@ -1,4 +1,16 @@
+/**
+ * ***********************************************
+ * File: Main.java
+ * Author: Parth Verma
+ * Description: This file contains code for the Main class (Reads from a file 
+ * and inputs to a HashTable)
+ * Date: July 14, 2022
+ * ***********************************************
+ */
+
+
 package unit7;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -10,25 +22,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         try {
             BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/Hashdata.txt")); //open file to read
-            String str;
-            String name;
-            String number;
-            String[] data;
+            String str; //to read each line
+            String name; //to be inputted
+            String number; //to be inputted
+            String[] data; //will contain name and number
             HashTable ht = new HashTable();
             while ((str = br.readLine()) != null) { //str will be set to null at the end of the file
-                data = str.split(";");
-                name = data[0].trim();
-                number = data[1].trim();
-                //System.out.println(name + " " + number + " " + ht.hashFunc(name));
+                data = str.split(";"); //split at the ";", as given in the data file
+                name = data[0].trim(); //trim off white-spaces
+                number = data[1].trim(); //trim off white-spaces
                 ht.add(name, number);
             }
-            String option = printMenu();
-            Scanner cont = null;
-            String answer;
+            String option = printMenu(); //print the menu from helper method
             boolean continueLoop = true;
+            
             while (continueLoop) {
                 switch (option) {
-                    case "1":
+                    
+                    case "1": //user selects to add a person
                         Scanner nameIn = new Scanner(System.in);
                         System.out.print("Please input the Name: ");
                         String newName = nameIn.nextLine();
@@ -37,24 +48,12 @@ public class Main {
                         String newNum = numIn.nextLine();
                         ht.add(newName, newNum);
                         
-                        if(detContinueLoop()) {
-                            option = printMenu();
-                            
-                        } else {
+                        if (detContinueLoop()) { //calls helper method to determine if code should continue running, or break the loop
+                            option = printMenu(); //saves user's new option choide
+                        } else { //if user says "N", terminate
                             continueLoop = false;
                             System.out.println("Thank you! Have a good day");
                         }
-                        /*
-                        cont = new Scanner(System.in);
-                        System.out.print("Would you like to continue? Y/N: ");
-                        answer = cont.nextLine();
-                        if(answer.equals("Y")) {
-                            option = printMenu();
-                        } else {
-                            System.out.println("Thank you! Have a good day");
-                            continueLoop = false;
-                        }
-                        */
                         break;
 
                     case "2":
@@ -63,69 +62,72 @@ public class Main {
                         String searchName = searchNameIn.nextLine();
                         System.out.println(ht.lookup(searchName));
                         
-                        cont = new Scanner(System.in);
-                        System.out.print("Would you like to continue? Y/N: ");
-                        answer = cont.nextLine();
-                        if (answer.equals("Y")) {
-                            option = printMenu();
-                        } else {
-                            System.out.println("Thank you! Have a good day");
+                        if (detContinueLoop()) { //calls helper method to determine if code should continue running, or break the loop
+                            option = printMenu(); //saves user's new option choide
+                        } else { //if user says "N", terminate
                             continueLoop = false;
+                            System.out.println("Thank you! Have a good day");
                         }
                         break;
 
                     case "3":
                         ht.printTable();
                         
-                        if (detContinueLoop()) {
-                            option = printMenu();
-
-                        } else {
+                        if (detContinueLoop()) { //calls helper method to determine if code should continue running, or break the loop
+                            option = printMenu(); //saves user's new option choide
+                        } else { //if user says "N", terminate
                             continueLoop = false;
                             System.out.println("Thank you! Have a good day");
                         }
                         break;
                         
-                    case "4":
+                    case "4": //autmoatically terminate
                         continueLoop = false;
                         System.out.println("Thank you! Have a good day");
-
                         break;
 
                     default:
                         throw new AssertionError();
                 } 
             }
-            //br.close(); //close file
+            br.close(); 
         } catch (Exception exc) {
             return;
         }
-        
     }
+    
+    /**
+     * ***********************************************
+     * @par Name printMenu
+     * @purpose A helper method to print out a menu for use with the HashTable and .txt data file
+     * @param [in] : None
+     * @return String***********************************************
+     */
     public static String printMenu() {
-
         Scanner input = new Scanner(System.in);
-
         System.out.println("\nPlease select a number option from the menu\n"
                 + "1. Add person and number to table\n"
                 + "2. Lookup a person by name in the Table\n"
                 + "3. Print table\n"
                 + "4. Quit\n");
-        String option = input.nextLine();
+        String option = input.nextLine(); //user input to be returned
         return option;
     }
+    
+    /**
+     * ***********************************************
+     * @par Name detContinueLoop
+     * @purpose A helper method to determine if the program should stop or run
+     * (depending on the Y/N input the user gives)
+     * @param [in] : None
+     * @return Boolean***********************************************
+     */
     public static boolean detContinueLoop () {
-        Scanner cont = null;
-        String answer;
-        boolean continueLoop = true;
-        cont = new Scanner(System.in);
+        Scanner cont = new Scanner(System.in);
         System.out.print("Would you like to continue? Y/N: ");
-        answer = cont.nextLine();
-        if (answer.equals("Y")) {
-            //option = printMenu();
+        if (cont.nextLine().equals("Y")) { //if user selects yes (to continue)
             return true;
         } else {
-            //System.out.println("Thank you! Have a good day");
             return false;
         }
     }
